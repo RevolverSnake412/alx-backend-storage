d2ced8584cdc9c33e9e290671ce231945af6a60e
RevolverSnake412 <oussag@hotmail.fr>
2024-04-26 00:11:09 +0000

    message

--- 0-uniq_users.sql
+++
@@ -1,8 +0,0 @@
--- 0-uniq_users.sql
--- creates a `users` table with `id`, `email`, `name`
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    PRIMARY KEY(id)
-);
